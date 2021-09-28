31627dd88bd95b8e3bad13daef77eee460922b09
imeneghazi <90001259+imeneghazi@users.noreply.github.com>
2021-09-28 16:17:10 -0400

    exercices faits

--- exercice.py
+++ exercice.py
@@ -8,16 +8,51 @@ import itertools
 
 
 def get_maximums(numbers):
-	return [0]
+	max_liste = []
+	for i in range(len(numbers)):
+		max_liste.append(max(numbers[i]))
+	return max_liste
 
 def join_integers(numbers):
-	return 0
+	nombre = ""
+	for i in range(len(numbers)):
+		nombre += str(numbers[i])
+	return int(nombre)
 
 def generate_prime_numbers(limit):
-	return [0]
+	premiers = []
+	nombres = []
+	j = 0
+	for i in range(2, limit+1):
+		nombres.append(i)
+	while nombres is not []:
+		is_prime = True
+		for d in range(2, nombres[j]//2):
+			if nombres[j] % d == 0:
+				is_prime = False
+				break
+		if is_prime:
+			premiers.append(nombres[j])
+		j += 1
+		if j == len(nombres):
+			break
+	return premiers
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [""]
+	Liste = []
+	nombre_combi = []
+	for k in range(1, num_combinations+1):
+		nombre_combi.append(k)
+	if excluded_multiples == None:
+		for i in range(num_combinations):
+			for j in range(len(strings)):
+				Liste.append(strings[j]+str(nombre_combi[i]))
+	else:
+		for i in range(1, num_combinations+1):
+			for j in range(len(strings)):
+				if i % excluded_multiples != 0:
+					Liste.append(strings[j]+str(nombre_combi[i-1]))
+	return Liste
 
 if __name__ == "__main__":
 	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
